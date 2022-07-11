c3eed32e8bf6816a08b719a75890d77ad3256d2d
Aurelien <linux.aurelien@gmail.com>
2022-07-11 11:59:00 +0200

    ajout psysh en DEV

--- migrations/Version20220711095149.php
+++ migrations/Version20220711095149.php
@@ -14,7 +14,7 @@ final class Version20220711095149 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Add date for Users';
     }
 
     public function up(Schema $schema): void
