346b3b22a9964fbc7b4090d5e96c1876863830df
Aurelien <linux.aurelien@gmail.com>
2022-08-11 11:03:12 +0200

    add show action and linked details

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
 use App\Repository\PostRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,5 +16,11 @@ class PostController extends AbstractController
         $posts = $postRepository->findAll();
 
         return $this->render('posts/index.html.twig', compact('posts'));
+    }
+
+    #[Route('/post/{slug}', name: 'app_posts_show')]
+    public function show(Post $post): Response
+    {
+        return $this->render('posts/show.html.twig', compact('post'));
     }
 }
