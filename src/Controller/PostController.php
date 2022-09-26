7d50535973a9a72c7cfb27d4997287687152cb72
Aurelien <linux.aurelien@gmail.com>
2022-09-26 14:50:45 +0200

    format requirements

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -22,7 +22,16 @@ class PostController extends AbstractController
         return $this->render('posts/index.html.twig', compact('posts'));
     }
 
-    #[Route('/post/{year<\d{4}>}/{month<\d{2}>}/{day<\d{2}>}/{slug<[a-z0-9\-]+>}', name: 'app_posts_show', methods: ['GET'])]
+    #[Route(
+        '/post/{year}/{month}/{day}/{slug}', 
+        requirements:[
+            'year' => '\d{4}',
+            'month' => '\d{2}',
+            'day' => '\d{2}'
+        ],
+        name: 'app_posts_show',
+        methods: ['GET'])
+     ]
     public function show(int $year,int $month,int $day,string $slug): Response
     {
         $post = $this->postRepository->findOneByPublishDateAndSlug($year,$month,$day,$slug);
